be98cb8751a1a1478336e10c759c3feb819b58cb
Valentin Rocher <vrocher@deezer.com>
2025-05-08 11:35:10 +0200

    Initial release
    Closes #1.
    
    Signed-off-by: Valentin Rocher <bishiboosh@gmail.com>

---
+++ Formula/caupain.rb
@@ -0,0 +1,45 @@
+class Caupain < Formula
+  desc "Your best buddy for keeping versions catalogs up to date!"
+  homepage "https://github.com/deezer/caupain"
+  url "https://github.com/deezer/caupain/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "66c590f573dca5ce2a5451b850165f89717622436d2836b0cb23fbaecb163a5f"
+  license "MIT"
+
+  depends_on "openjdk@17" => :build
+
+  def install
+    ENV["GRADLE_OPTS"] = '-Dorg.gradle.configureondemand=true \
+    -Dkotlin.incremental=false \
+    -Dorg.gradle.project.kotlin.incremental.multiplatform=false \
+    -Dorg.gradle.project.kotlin.native.disableCompilerDaemon=true \
+    -Dorg.gradle.jvmargs="-Xmx12g \
+    -Dfile.encoding=UTF-8"'
+    task =
+      if Hardware::CPU.arm?
+        ":cli:macosArm64Binaries"
+      else
+        ":cli:macosX64Binaries"
+      end
+    system "./gradlew", task, "--no-configuration-cache"
+    folder =
+      if Hardware::CPU.arm?
+        "macosArm64"
+      else
+        "macosX64"
+      end
+    bin.install "cli/build/bin/#{folder}/releaseExecutable/caupain.kexe" => "caupain"
+    bash_completion.install "cli/completions/bash-completion.sh" => "caupain"
+    fish_completion.install "cli/completions/fish-completion.sh"
+    zsh_completion.install "cli/completions/zsh-completion.sh" => "_caupain"
+  end
+
+  test do
+    resource "testcatalog" do
+      url "https://raw.githubusercontent.com/deezer/caupain/refs/heads/main/cli/tests/libs.versions.toml"
+      sha256 "0438c2ab962721021790e4fea4ead214b91c5019119593a801c9da562ae374ac"
+    end
+    resource("testcatalog").stage do
+      shell_output("#{bin}/caupain --verbose -i libs.versions.toml")
+    end
+  end
+end
---
+++ README.md
@@ -0,0 +1,18 @@
+# Deezer Homebrew Tap
+
+## How do I install these formulae?
+
+`brew install deezer/repo/<formula>`
+
+Or `brew tap deezer/repo` and then `brew install <formula>`.
+
+Or, in a `brew bundle` `Brewfile`:
+
+```ruby
+tap "deezer/repo"
+brew "<formula>"
+```
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
