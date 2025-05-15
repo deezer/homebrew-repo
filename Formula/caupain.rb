0f25deda468c336d6e05bb6d7902e2e41d0e4f7e
Valentin Rocher <vrocher@deezer.com>
2025-05-15 11:24:22 +0200

    Update to 1.0.1

--- Formula/caupain.rb
+++ Formula/caupain.rb
@@ -1,15 +1,9 @@
 class Caupain < Formula
   desc "Your best buddy for keeping versions catalogs up to date!"
   homepage "https://github.com/deezer/caupain"
-  url "https://github.com/deezer/caupain/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "19fa9c839e4a39ced552062099705e3bedd91360c9732b4ba3af0b318655697d"
+  url "https://github.com/deezer/caupain/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "99672ceb6c26426e334f7dd2bfaf59df4b2b6226be9d6e91b02c425b4712c1fa"
   license "MIT"
-
-  bottle do
-    root_url "https://github.com/deezer/homebrew-repo/releases/download/caupain-1.0.0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d715ae15475311752f2d0c12c9f9f61a71c54d46b3852beb730bd2350a4a1fe1"
-    sha256 cellar: :any_skip_relocation, ventura:       "cac16ca5db03fe794af1e2340f0c072b85186458597816685796d382526d3648"
-  end
 
   depends_on "openjdk@17" => :build
 
