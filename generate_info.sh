0ce3de757d97888c484c0c6d427c4606fb33d2af
Valentin Rocher <vrocher@deezer.com>
2025-05-25 16:56:39 +0200

    Fix style again
    Closes #6.
    
    Signed-off-by: Valentin Rocher <bishiboosh@gmail.com>

--- generate_info.sh
+++ generate_info.sh
@@ -3,7 +3,8 @@
 rm -rf Info
 mkdir -p Info
 cd Formula || exit
-for formula in *.rb; do
-    JSON_INFO=$(brew info --json "${formula}")
-    echo $JSON_INFO | jq '.[0]? // .' > ../Info/"${formula/%rb/json}"
+for formula in *.rb
+do
+  JSON_INFO=$(brew info --json "${formula}")
+  echo "${JSON_INFO}" | jq '.[0]? // .' >../Info/"${formula/%rb/json}"
 done
